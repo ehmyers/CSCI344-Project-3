b2662e0aec6f41269d541be300a71c9273657b70
Emily Myers <emilyhmyers@gmail.com>
2013-03-10 23:05:29 -0400

    fix icon-remove

--- javascripts/app/app.js
+++ javascripts/app/app.js
@@ -47,7 +47,7 @@ var fillCategory = function (category_name, itemIndex) {
             // checks if the item's category matches the current category
             if (category === category_name) {
                 // adds the item to a paragraph
-                $("<p data-id='" + todo.id + "'><i class='icon_remove'></i>" + todo.description + "</p>").appendTo("#" + category_name);
+                $("<p data-id='" + todo.id + "'><i class='icon-remove'></i>" + todo.description + "</p>").appendTo("#" + category_name);
             }
             if (categoryNames.indexOf(category) === -1) {
                 addUnseenCategoryToArray(category);
@@ -65,7 +65,7 @@ var refreshMainList = function () {
     $("#all-items").empty();
     todos.forEach(function (todo) {
         // adds a paragraph, adds the remove button to the paragraph
-        list_item = "<p id='title_and_category' data-id='" + todo.id + "'><i class='icon_remove'></i>";
+        list_item = "<p id='title_and_category' data-id='" + todo.id + "'><i class='icon-remove'></i>";
         // adds each description to the string
         list_item += todo.description + "<span id='categories'>";
         // adds each category to the string
@@ -203,7 +203,7 @@ var main = function () {
         todos = json_todos;
         initializeTodos();
         refreshMainList();
-        $(".icon_remove").click(removeItem);
+        $(".icon-remove").click(removeItem);
         $("#tab_categorized").click(refreshCategorizedList);
         $(".user_input_button").click(editTab);
         $(".user_input").keypress(submitOnEnter);
